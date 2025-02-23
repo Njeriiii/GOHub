9106c3dad99921ec1f29a5feb29d43531c018f87
Njeriiii <joymwenjwa@gmail.com>
2025-02-23 01:35:48 -0800

    remove shadow in Header

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -50,7 +50,7 @@ export default function Header() {
     };
 
     return (
-        <header className="bg-white shadow-md">
+        <header className="bg-white">
         <nav
             className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8"
             aria-label="Global"
